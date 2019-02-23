be92ad2fa9ad9ad429ca61f4a4323d7d93e53168
Nicolás Gonzálvez <n.gonzalvez@outlook.com>
2019-02-23 03:26:48 -0300

    Fix git clone command.

---
+++ README.md
@@ -0,0 +1,10 @@
+# dotfiles
+
+### Install
+
+```bash
+wget https://raw.githubusercontent.com/ngonzalvez/dot-files/master/setup.sh | sh
+```
+
+
+
--- setup.sh
+++ setup.sh
@@ -1,2 +1,5 @@
-git clone git@github.com:ngonzalvez/dot-files.git -C $HOME/
-rm -rf $HOME/.git
+#!/bin/bash
+
+git clone git@github.com:ngonzalvez/dot-files.git $HOME/.dotfiles
+
+mv $HOME/.dotfiles/* $HOME/
